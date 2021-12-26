f2c58d3a019bc19db1e39733d3f68cc932506d2b
Maycon J Pereira <mayconjova@gmail.com>
2021-12-26 17:06:02 -0300

    Added menu components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { MoviesComponent } from './views/movies/movies.component';
+import { SeriesComponent } from './views/series/series.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent
+},
+{
+  path: "movies",
+  component: MoviesComponent
+},
+{
+  path: "series",
+  component: SeriesComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,24 +4,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+
 import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavigationComponent } from './components/template/navigation/navigation.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {MatIconModule} from '@angular/material/icon';
-
-
+import { HomeComponent } from './views/home/home.component';
+import { MoviesComponent } from './views/movies/movies.component';
+import { SeriesComponent } from './views/series/series.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    HomeComponent,
+    MoviesComponent,
+    SeriesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
